8b7a3edfdb809d516c4a2fdc48734b0332759aae
Vanex-h <vanessahirwa5@gmail.com>
2024-02-11 22:16:20 +0200

    Displaying thr thresholded and sharpened images

--- readBlurry.py
+++ readBlurry.py
@@ -10,12 +10,12 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 sharpen_kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
 sharpen = cv2.filter2D(gray, -1, sharpen_kernel)
 thresh = cv2.threshold(sharpen, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-
+thresh2 = cv2.threshold(sharpen, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 # ocr
-data = pytesseract.image_to_string(thresh, lang='eng', config='--psm 6')
+data = pytesseract.image_to_string(thresh2,lang='eng', config='--psm 6')
 
-cv2.imshow('sharpen', sharpen)
-cv2.imshow('thresh', thresh)
+# cv2.imshow('sharpen', sharpen)
+cv2.imshow('thresh', thresh2)
 cv2.waitKey()
 numbers = ''.join(filter(str.isdigit, data))
 
